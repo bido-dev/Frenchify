767ce2a314895a6f751d129d44d631e39f8f04dd
bido-dev <bido.mohammedmohsen@gmail.com>
2026-02-02 12:24:59 +0300

    testdeploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Frenchify</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count sooooooo {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
